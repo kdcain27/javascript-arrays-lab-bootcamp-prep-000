661c45e2cea14b8abc7dee17ab869e3dd05e83ea
Kevin Krause <kkrausesc@gmail.com>
2017-09-10 12:36:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kittens.pop(name)
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift(name)
+}
+
+function appendKitten(name) {
+  return [...kittens,name]
+}
+
+function prependKitten(name) {
+  return [name,...kittens]
+}
+
+function removeLastKitten() {
+  return = kittens.slice(- 1)
+}
+
+function removeFirstKitten() {
+  newArray = kittens.slice(1)
+  return newArray
+}
